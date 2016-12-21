ece1b59b1cccb882a152264672b68117294dfca1
Marc Nützel <marc.nuetzel@me.com>
2016-12-21 09:07:54 +0100

    initial

---
+++ connection_tester.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import csv
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+with open('connections.csv', 'r') as f:
+  reader = csv.reader(f)
+  for row in reader:
+        result = sock.connect_ex((row[0],int(row[1])))
+        if result != 0:
+            print "Not working: " + row[0] + ":" + row[1]   
